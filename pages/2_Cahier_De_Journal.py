a9bef5f22b49863a5e94c8d734aa4a4cc79881ed
ABDESSAMAD TAOUFQ <110288424+GitSamad88@users.noreply.github.com>
2023-11-30 23:55:59 +0100

    Update 2_Cahier_De_Journal.py

--- pages/2_Cahier_De_Journal.py
+++ pages/2_Cahier_De_Journal.py
@@ -764,3 +764,5 @@ if (teaching_language == "Français"):
 
 elif (teaching_language == "Arabe"):
     form.warning("La création du cahier de journal de la langue arabe est impossible pour le moment!")
+
+col2.markdown('<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4265574502229447"crossorigin="anonymous"></script>')
