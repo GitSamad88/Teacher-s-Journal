b75db3ff09b70e1136b988caf1ac8e181e1f1f42
ABDESSAMAD TAOUFQ <110288424+GitSamad88@users.noreply.github.com>
2023-12-01 22:14:31 +0100

    Update Page_d_acceuil.py

--- Page_d_acceuil.py
+++ Page_d_acceuil.py
@@ -7,7 +7,34 @@ st.set_page_config(
     page_title="Multipage App")
 
 
-st.markdown("""<meta name="google-adsense-account" content="ca-pub-4265574502229447">""",unsafe_allow_html=True)
+GA_ID = "google_analytics"
+ga_script = """<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-2TE23YZQ28"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-2TE23YZQ28');
+</script> """
+ads_tag = """<meta name="google-adsense-account" content="ca-pub-4265574502229447">"""
+
+def inject_ga():
+    index_path = pathlib.Path(st.__file__).parent / "static" / "index.html"
+    soup = BeautifulSoup(index_path.read_text(), features="html.parser")
+    if not soup.find(id=GA_ID):
+        bck_index = index_path.with_suffix('.bck')
+        if bck_index.exists():
+            shutil.copy(bck_index, index_path)
+        else:
+            shutil.copy(index_path, bck_index)
+        html = str(soup)
+        new_html = html.replace('<head>', '<head>\n' + ga_script + ads_tag)
+        index_path.write_text(new_html)
+
+
+inject_ga()
+
 
 link = "This app is made by [TAOUFIQ ABDESSAMAD](https://www.linkedin.com/in/abdessamad-taoufiq-082013209/)"
 st.sidebar.markdown(link,unsafe_allow_html=True)
