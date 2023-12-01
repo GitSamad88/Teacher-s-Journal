f8ec62d8b37ca4ab3da51fd502a6411c4b84fab1
ABDESSAMAD TAOUFQ <110288424+GitSamad88@users.noreply.github.com>
2023-12-01 22:17:21 +0100

    Update 2_Cahier_De_Journal.py

--- pages/2_Cahier_De_Journal.py
+++ pages/2_Cahier_De_Journal.py
@@ -23,8 +23,36 @@ import json
 warnings.filterwarnings("ignore")
 
 # connect google adsense
-st.write("ads")
-st.markdown("""<meta name="google-adsense-account" content="ca-pub-4265574502229447">""",unsafe_allow_html=True)
+GA_ID = "google_analytics"
+ga_script = """<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-2TE23YZQ28"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-2TE23YZQ28');
+</script> """
+ads_tag = """<meta name="google-adsense-account" content="ca-pub-4265574502229447">"""
+
+def inject_ga():
+    index_path = pathlib.Path(st.__file__).parent / "static" / "index.html"
+    soup = BeautifulSoup(index_path.read_text(), features="html.parser")
+    if not soup.find(id=GA_ID):
+        bck_index = index_path.with_suffix('.bck')
+        if bck_index.exists():
+            shutil.copy(bck_index, index_path)
+        else:
+            shutil.copy(index_path, bck_index)
+        html = str(soup)
+        new_html = html.replace('<head>', '<head>\n' + ga_script + ads_tag)
+        index_path.write_text(new_html)
+
+
+inject_ga()
+
+
+
 def fr_manuel_level(manuel, level):
     # mes_app1 = pd.read_csv(r"C:\Users\hp\Downloads\edited repartitions\fr repa\Mes apprentissage\Repa  fr dire faire 1aep final.csv")  # to be changed!
     mes_app1 = pd.read_csv(
