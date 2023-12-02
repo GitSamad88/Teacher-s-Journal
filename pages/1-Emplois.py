37dfcb8615bb317ca077483779998bb76c695e85
ABDESSAMAD TAOUFQ <110288424+GitSamad88@users.noreply.github.com>
2023-12-02 23:20:56 +0100

    Update 1-Emplois.py

--- pages/1-Emplois.py
+++ pages/1-Emplois.py
@@ -2,38 +2,11 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import os
-from bs4 import BeautifulSoup
-import pathlib
-import shutil
+#from bs4 import BeautifulSoup
+#import pathlib
+#import shutil
 from io import BytesIO
 
-# connect google adsense
-GA_ID = "google_analytics"
-ga_script = """<!-- Google tag (gtag.js) -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-2TE23YZQ28"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'G-2TE23YZQ28');
-</script> """
-ads_tag = """<meta name="google-adsense-account" content="ca-pub-4265574502229447">"""
-
-def inject_ga():
-    index_path = pathlib.Path(st.__file__).parent / "static" / "index.html"
-    soup = BeautifulSoup(index_path.read_text(), features="html.parser")
-    if not soup.find(id=GA_ID):
-        bck_index = index_path.with_suffix('.bck')
-        if bck_index.exists():
-            shutil.copy(bck_index, index_path)
-        else:
-            shutil.copy(index_path, bck_index)
-        html = str(soup)
-        new_html = html.replace('<head>', '<head>\n' + ga_script + ads_tag)
-        index_path.write_text(new_html)
-        
-inject_ga()
 
 
 buffer = BytesIO()
