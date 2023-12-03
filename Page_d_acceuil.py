f15ff64c95594f46dfbc2ac2fc9b49a6d472dd2e
ABDESSAMAD TAOUFQ <110288424+GitSamad88@users.noreply.github.com>
2023-12-03 15:23:47 +0100

    Update Page_d_acceuil.py

--- Page_d_acceuil.py
+++ Page_d_acceuil.py
@@ -8,6 +8,32 @@ import shutil
 
 st.set_page_config(
     page_title="Multipage App")
+GA_ID = "google_analytics"
+ga_script = """<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-14J0CKQ261"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-14J0CKQ261');
+</script>"""
+
+def inject_ga():
+    index_path = pathlib.Path(st.__file__).parent / "static" / "index.html"
+    soup = BeautifulSoup(index_path.read_text(), features="html.parser")
+    if not soup.find(id=GA_ID):
+        bck_index = index_path.with_suffix('.bck')
+        if bck_index.exists():
+            shutil.copy(bck_index, index_path)
+        else:
+            shutil.copy(index_path, bck_index)
+        html = str(soup)
+        new_html = html.replace('<head>', '<head>\n' + ga_script)
+
+
+
+inject_ga()
 
 
 link = "This app is made by [TAOUFIQ ABDESSAMAD](https://www.linkedin.com/in/abdessamad-taoufiq-082013209/)"
