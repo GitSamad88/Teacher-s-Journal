69dc901e3aee36c06abc6add039d42b0830fe7ce
ABDESSAMAD TAOUFQ <110288424+GitSamad88@users.noreply.github.com>
2023-12-04 14:03:23 +0100

    Update Home.py

--- Home.py
+++ Home.py
@@ -9,7 +9,50 @@ import shutil
 st.set_page_config(
     page_title="Multipage App")
 
+# google ads and analytics scripts:
 
+g_ads = """<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4265574502229447"
+     crossorigin="anonymous"></script>"""
+
+GA_ID = "google_analytics"
+ga_script = """<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-2TE23YZQ28"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-2TE23YZQ28');
+</script> """
+
+def inject_ga():
+    index_path = pathlib.Path(st.__file__).parent / "static" / "index.html"
+    soup = BeautifulSoup(index_path.read_text(), features="html.parser")
+
+    if ("Google tag" not in str(soup)):
+        bck_index = index_path.with_suffix('.bck')
+        if bck_index.exists():
+            shutil.copy(bck_index, index_path)
+        else:
+            shutil.copy(index_path, bck_index)
+        html = str(soup)
+        new_html = html.replace('<head>', '<head>\n' + ga_script + g_ads)
+        index_path.write_text(new_html)
+    else :
+        bck_index = index_path.with_suffix('.bck')
+        if bck_index.exists():
+            shutil.copy(bck_index, index_path)
+        else:
+            shutil.copy(index_path, bck_index)
+        html = str(soup)
+        html = html.replace(ga_script, "")
+        html = html.replace(g_ads,"")
+        new_html = html.replace('<head>', '<head>\n' + ga_script + g_ads)
+        index_path.write_text(new_html)
+       
+
+
+inject_ga()
 
 
 link = "This app is made by [TAOUFIQ ABDESSAMAD](https://www.linkedin.com/in/abdessamad-taoufiq-082013209/)"
@@ -66,51 +109,5 @@ col1.write("Téléchargez Le Cahier Journalier Educatif dès aujourd'hui et déc
          " la façon dont les enseignants du cycle primaire gèrent leurs cahiers de journal, "
          "libérant ainsi du temps précieux pour se concentrer sur "
          "l'enseignement et l'épanouissement de leurs élèves.")
-
-
-# google ads and analytics scripts:
-
-g_ads = """<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4265574502229447"
-     crossorigin="anonymous"></script>"""
-
-GA_ID = "google_analytics"
-ga_script = """<!-- Google tag (gtag.js) -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-2TE23YZQ28"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'G-2TE23YZQ28');
-</script> """
-
-def inject_ga():
-    index_path = pathlib.Path(st.__file__).parent / "static" / "index.html"
-    soup = BeautifulSoup(index_path.read_text(), features="html.parser")
-
-    if ("Google tag" not in str(soup)):
-        bck_index = index_path.with_suffix('.bck')
-        if bck_index.exists():
-            shutil.copy(bck_index, index_path)
-        else:
-            shutil.copy(index_path, bck_index)
-        html = str(soup)
-        new_html = html.replace('<head>', '<head>\n' + ga_script + g_ads)
-        index_path.write_text(new_html)
-    else :
-        bck_index = index_path.with_suffix('.bck')
-        if bck_index.exists():
-            shutil.copy(bck_index, index_path)
-        else:
-            shutil.copy(index_path, bck_index)
-        html = str(soup)
-        html = html.replace(ga_script, "")
-        html = html.replace(g_ads,"")
-        new_html = html.replace('<head>', '<head>\n' + ga_script + g_ads)
-        index_path.write_text(new_html)
-       
-
-
-inject_ga()
 
 
